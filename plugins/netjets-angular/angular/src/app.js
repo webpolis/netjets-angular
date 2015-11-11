9dc05c2510f5261b521261a28f3c893478f5bcc7
Nicolas Iglesias <nico@webpolis.com.ar>
2015-11-11 00:54:16 -0300

    secure websocket

--- plugins/netjets-angular/angular/src/app.js
+++ plugins/netjets-angular/angular/src/app.js
@@ -1,4 +1,4 @@
 angular.module('netjets', ['ngSanitize', 'netjets.services', 'netjets.directives']).config(function(questionsSvcProvider){
-	questionsSvcProvider.wsEndpoint = 'ws://test1.cloud.answerhub.com/api/socket/';
+	questionsSvcProvider.wsEndpoint = 'wss://test1.cloud.answerhub.com:443/api/socket/';
 	questionsSvcProvider.container = 7;
 });
